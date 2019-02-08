db7bed90d92ea2b521a762541d7e5bbca5ba8d86
pantitij <Pantiti@hotmail.com>
2019-02-08 21:44:45 +0700

    <add service for feedback UI - close #93>

---
+++ frontend/src/app/service/allservice2.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Allservice2Service {
+  public API = '//localhost:8080';
+
+
+  constructor(private httpClient:HttpClient) { }
+
+  getTaste():Observable<any>{
+    return this.httpClient.get(this.API+'/taste');
+  }
+
+  getService():Observable<any>{
+    return this.httpClient.get(this.API+'/service');
+  }
+
+  getVibe():Observable<any>{
+    return this.httpClient.get(this.API+'/vibe');
+  }
+
+  getPrice():Observable<any>{
+    return this.httpClient.get(this.API+'/price');
+  }
+
+  getFeedback():Observable<any>{
+    return this.httpClient.get(this.API+'/feedback');
+  }
+
+}
