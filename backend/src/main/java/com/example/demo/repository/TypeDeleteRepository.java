7948afd62e8510a3a431a22688f35afe900ab087
nnamphon <nongnamphonlunla@gmail.com>
2019-02-09 12:51:02 +0000

    add repository - close #89

---
+++ backend/src/main/java/com/example/demo/repository/TypeDeleteRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.TypeDelete;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@RepositoryRestResource
+@CrossOrigin(origins = "http://localhost:4200")
+public interface TypeDeleteRepository extends JpaRepository<TypeDelete,Long> {
+    
+}
