c1360e7547716d95ce7df0dd632fd8a43e04ff7e
mapang2539 <naphatsawan2755@gmail.com>
2019-02-10 12:16:06 +0700

    add test - close #102

---
+++ backend/src/test/java/com/example/demo/PaymentTest.java
@@ -0,0 +1,154 @@
+package com.example.demo;
+
+import com.example.demo.entity.Creditcardpayment;
+import com.example.demo.entity.Payment;
+import com.example.demo.entity.Staff;
+import com.example.demo.repository.CreditcardpaymentRepository;
+import com.example.demo.repository.PaymentRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class PaymentTest {
+
+
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    @Autowired
+    private TestEntityManager entityManager;
+    private Validator validator;
+    @Before
+    public void setup() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+   //------------------------Sprint1-----------------------------------//
+
+    @Test
+    //test null
+    public void testNoteCannotNull() {
+        Payment payment = new Payment();
+        payment.setNote(null);
+        payment.setMoney(20);
+        try {
+            entityManager.persist(payment);
+            entityManager.flush();
+            fail("note is null");
+        } catch (javax.validation.ConstraintViolationException e) {
+            System.out.println("================================ Note Cannot Null ==================");
+            System.out.println(e.getConstraintViolations());
+            Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+            assertEquals(violations.isEmpty(), false);
+            assertEquals(violations.size(), 1);
+        }
+    }
+
+    @Test
+    //min note
+    public void testSizeMinNote() {
+        Payment payment = new Payment();
+        payment.setNote("O");
+        payment.setMoney(20);
+        try {
+            entityManager.persist(payment);
+            entityManager.flush();
+            fail("Note too less character");
+        } catch(javax.validation.ConstraintViolationException e) {
+            System.out.println("================================ Noteminsize ==================");
+            System.out.println(e.getConstraintViolations());
+            Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+            assertEquals(violations.isEmpty(), false);
+            assertEquals(violations.size(), 1);
+
+        }
+    }
+
+    @Test
+    //max note
+    public void testSizeMaxNote() {
+        Payment payment = new Payment();
+        payment.setNote("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+        payment.setMoney(20);
+        try {
+            entityManager.persist(payment);
+            entityManager.flush();
+            fail("Note too less character");
+        } catch(javax.validation.ConstraintViolationException e) {
+            System.out.println("================================ Notemaxsize ==================");
+            System.out.println(e.getConstraintViolations());
+            Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+            assertEquals(violations.isEmpty(), false);
+            assertEquals(violations.size(), 2);
+
+        }
+    }
+
+    @Test
+    //pattern note
+    public void testPatternNote() {
+        Payment payment = new Payment();
+        payment.setNote("napat");
+        payment.setMoney(20);
+        try {
+            entityManager.persist(payment);
+            entityManager.flush();
+            fail("Note pattern is ascii");
+        } catch(javax.validation.ConstraintViolationException e) {
+            System.out.println("================================ Note Pattern is thai and 0-9 ==================");
+            System.out.println(e.getConstraintViolations());
+            Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+            assertEquals(violations.isEmpty(), false);
+            assertEquals(violations.size(), 1);
+
+        }
+    }
+
+//    @Test
+//    public void testStaffUnique() {
+//        Staff s = new Staff();
+//        s.setStaffName("นภัสวรรณ");
+//        entityManager.persist(s);
+//        entityManager.flush();
+//
+//        Staff s1 = new Staff();
+//        s1.setStaffName("นภัสวรรณ");
+//
+//        try{
+//            entityManager.persist(s1);
+//            entityManager.flush();
+//
+//            //fail("Should not pass to this line");
+//        } catch(javax.validation.ConstraintViolationException e) {
+//            System.out.println("================Staff is Notnull ======================");
+//            e.printStackTrace();
+//            Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+//            assertEquals(violations.isEmpty(), false);
+//            assertEquals(violations.size(), 1);
+//        }
+//        catch (javax.persistence.PersistenceException e){
+//            System.out.println("==================================================================");
+//            e.printStackTrace();
+//        }
+//    }
+
+
+
+
+}
